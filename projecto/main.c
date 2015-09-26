8f8acc310e41b2b302a60382aa11615776d0fe91
fatherboard <pedro.bucho@gmail.com>
2015-09-26 16:59:32 +0100

    Progresso no forking dos processos

--- projecto/main.c
+++ projecto/main.c
@@ -1,32 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "commandlinereader.h"
 
-#define VECTOR_SIZE 5
+#define VECTOR_SIZE 6
 #define ARG_LEN 100
 
 int main(int argc, char *argv[]){
 	
-	int i;
-	char input[ARG_LEN], command[ARG_LEN];
 	char **argVector;
 	
+	argVector = (char **) malloc(VECTOR_SIZE * sizeof(char*));
+	
 	while(1){
-		fgets(input, ARG_LEN, stdin);
-		sscanf(input, "%s", command);
+		/*fgets(input, ARG_LEN, stdin);
+		sscanf(input, "%s", command);*/
+		readLineArguments(argVector, VECTOR_SIZE);
 		
-		if(strcmp(command, "pathname") == 0){
-			// enviar os comandos
-			argVector = (char **) malloc(VECTOR_SIZE * sizeof(char*));
-			readLineArguments(argVector, VECTOR_SIZE);
-		}else if(strcmp(command, "exit") == 0){
-			free(argVector);
+		if(strcmp(argVector[0], "exit") == 0){
 			break;
 		}else{
-			printf("Command not found.\n");
+			// qualquer comando que nao seja "exit" e considerado como
+			// um comando para ser procurado na directoria de trabalho
+			// e executado
+			
+			int status, pid = fork();
+			
+			if(pid != 0){
+				// pai
+				if(pid < 0){
+					// erro ao criar o processo filho
+					//write(stdout, "Error creating process\n", 23);
+					perror("Error forking process\n");
+				}
+				wait(&status);
+				
+				// neste exercicio o pai nao monitoriza os filhos durante
+				// a execucao, apenas quando termina
+			}else{
+				// filho
+				// substitui a imagem do executavel actual
+				// pelo especificado no comando
+				if(execv(argVector[0], argVector)){
+					perror("Error in execv\n");
+				}
+			}
+			// espirito santo? novo banco?
 		}
 	}
+	
+	
+	
+	free(argVector);
 	
 	exit(EXIT_SUCCESS);
 }
