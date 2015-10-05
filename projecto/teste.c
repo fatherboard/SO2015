3327744f4ce22bbb07b057ca26df7a51aebca039
Miguel Amaral <miguel.amaral04@gmail.com>
2015-10-05 12:34:51 +0100

    adicionadas funçoes de teste

--- projecto/teste.c
+++ projecto/teste.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+	/**
+	 * @description: function that prints the arguments in argVector
+	 */
 int main(int argc, char *argv[]){
 
 	if(argc > 1){
 		int i;
 		printf("TESTE: ");
-		for(i = 0; i < argc; i++){
+		for(i = 1; i < argc; i++){
 			printf("%s ", argv[i]);
 		}
 		printf("\n");
@@ -15,5 +18,4 @@ int main(int argc, char *argv[]){
 		printf("TESTE: <sem args>\n");
 
 	exit(EXIT_SUCCESS);
-
 }
--- projecto/time_test.c
+++ projecto/time_test.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+ * @description: program that sleeps for the amount of time in argv 
+ */
 int main(int argc, char *argv[]){
 
-        if(argc > 1){
+	int sum_time = 0;
+  if(argc > 1){
 
 		int i;
-		int time = 0;
 		printf("Times: \n");
-		for(i = 0; i < argc; i++){
-			time = time + atoi(argv[i]);
-		}
-		printf("Will sleep %d seconds\n",time);
-		sleep(time);
-		printf("Sleept %d seconds\n",time);
+		for(i = 0; i < argc; i++)
+			sum_time = sum_time + atoi(argv[i]);
+
+		printf("Process %d Will sleep %d seconds\n",(int) getpid(),sum_time);
+		sleep(sum_time);
 	}
-	else
+	else{
 		printf("No time given: <sem args>\n");
-  printf("Process %d finished sleeping.\n", (int) getpid());
+    exit(10);
+  }
+  printf("Process %d finished sleeping: %d seconds.\n", (int) getpid(), sum_time);
   exit(EXIT_SUCCESS);
 }
